6bcb66d0fc503e110c47791aa167902b5a03f541
SongYanglin <1503464667@qq.com>
2022-02-20 15:50:41 +0800

    完成基础项目搭建

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// 单元测试配置https://juejin.cn/post/7039146473878978596
+// eslint 配置 https://juejin.cn/post/6975442828386107400#comment
+// https://juejin.cn/post/7002869322737516581#heading-7
